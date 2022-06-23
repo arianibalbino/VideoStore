1a70371a6b53021330b29a7ec1788681577244f3
Ariani Assunção <41275727+arianibalbino@users.noreply.github.com>
2022-06-23 02:53:56 +0000

    Commit 3

--- Customer.java
+++ Customer.java
@@ -17,22 +17,22 @@ public class Customer {
       return _name; 
    }
 
- private double amountFor(Rental each) {
+   private double amountFor(Rental aRental) {
         double thisAmount = 0;
         // determine amounts for each line
-        switch (each.getMovie().getPriceCode()) {
+        switch (aRental.getMovie().getPriceCode()) {
             case Movie.REGULAR:
                 thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                if (aRental.getDaysRented() > 2)
+                    thisAmount += (aRental.getDaysRented() - 2) * 1.5;
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+                thisAmount += aRental.getDaysRented() * 3;
                 break;
             case Movie.CHILDRENS:
                 thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                if (aRental.getDaysRented() > 3)
+                    thisAmount += (aRental.getDaysRented() - 3) * 1.5;
                 break;
         }
 
@@ -48,6 +48,7 @@ public class Customer {
      while (rentals.hasMoreElements()) {
        
         Rental each = (Rental) rentals.nextElement();
+            double thisAmount = amountFor(each);
         
       
 
@@ -60,7 +61,6 @@ public class Customer {
         //show figures for this rental
         result += "\t" + each.getMovie().getTitle()+ "\t" +
             String.valueOf(thisAmount) + "\n";
-            double thisAmount = amountFor(each);
             totalAmount += thisAmount;
 
      }
