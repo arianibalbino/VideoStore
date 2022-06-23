9751a9360396ff571b9a758de0e01d3b202f51c4
Ariani Assunção <41275727+arianibalbino@users.noreply.github.com>
2022-06-23 03:22:07 +0000

    Commit 10

--- Movie.java
+++ Movie.java
@@ -42,4 +42,11 @@ public class Movie {
         }
         return thisAmount;
     }
+      public int getFrequentRenterPoints(int daysRented) {
+        if (this.getPriceCode() == NEW_RELEASE && daysRented > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
 }
